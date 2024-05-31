03657d403f6b2696b9f0a128862724b5fda4a36b
praveenwedda <praveenwedda1992@gmail.com>
2024-05-31 20:18:35 +0530

    played around with os built in module, commented out all previous work in app.js

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 //setting up the required files to run the code below
-const requiredModule1 = require("./namer.js");
-const requiredModule2 = require("./names.js");
+// const requiredModule1 = require("./namer.js");
+// const requiredModule2 = require("./names.js");
 
-console.log(requiredModule1);
-console.log(requiredModule2);
+// console.log(requiredModule1);
+// console.log(requiredModule2);
 
-requiredModule1.namer(requiredModule2.name1);
-// namer(name2);
-// namer(name3);
+// requiredModule1.namer(requiredModule2.name1);
+// // namer(name2);
+// // namer(name3);
+
+const os = require("os");
+
+const userinfo = os.userInfo();
+console.log(userinfo);
+console.log(os.uptime());
+
+const currentsystemstat = {
+  name: os.type(),
+  release: os.release(),
+  totalMemory: os.totalmem(),
+  freeMemory: os.freemem(),
+};
+
+console.log(currentsystemstat);
