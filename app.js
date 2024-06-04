c4cf2cc89fb5dcbd0e7dbf375a64fc0878e4c397
praveenwedda <praveenwedda1992@gmail.com>
2024-06-04 11:53:50 +0530

    reading and writing from and to files using fs module play around

--- app.js
+++ app.js
@@ -48,17 +48,31 @@
 
 //-------------Above is how we recurse to get fibonacci series--------------------------
 
-const k = require("path");
-// console.log(k);
+// const k = require("path");
+// // console.log(k);
 
-const c = k.sep;
-// console.log(c);
+// const c = k.sep;
+// // console.log(c);
 
-const a = k.resolve(__dirname, "app.js");
-// console.log(a);
+// const a = k.resolve(__dirname, "app.js");
+// // console.log(a);
 
-const b = k.join("app.js", "namer.js"); //put a directory inside another
-// console.log(b);
+// const b = k.join("app.js", "namer.js"); //put a directory inside another
+// // console.log(b);
 
-const f = k.basename(b);
-console.log(f);
+// const f = k.basename(b);
+// console.log(f);
+//----------------------Above is everything we did in terms of path module--------------
+
+const { readFileSync, writeFileSync } = require("fs");
+
+const r1 = readFileSync("./content/text.txt", "utf8");
+console.log(r1);
+
+const p = "Praveen will be a great coder in the name of Jesus!";
+
+writeFileSync("./content/result.txt", p);
+const r2 = readFileSync("./content/result.txt", "utf8"); //when you are writing to a file, the content it has prior to writing attempt gets erased pr
+console.log(r2);
+
+//-------------Above is about reading and writing to files using fs module------------
