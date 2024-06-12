48e89b8c9c4e71cd0976eda4e84e56278720aa69
Praveen W <praveenwedda1992@gmail.com>
2024-06-12 09:32:16 -0500

    syncing 2 laps

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ const { readFile, writeFile } = require("fs");
 const  util  = require("util");
 const readFilePromise = util.promisify(readFile)
 const writeFilePromise = util.promisify(writeFile)
-
+//
 
 const start = async() => {
   try {
